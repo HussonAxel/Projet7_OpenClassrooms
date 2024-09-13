b91ab59c6c4c0c397fca2e2b15b59ab50ab1ddca
axel husson <aaaxel.hsson@gmail.com>
2024-09-13 11:06:42 +0200

    fix responsive issue on header logo and html issues for validator

--- src/scripts/types/Recipe.ts
+++ src/scripts/types/Recipe.ts
@@ -1,18 +1,16 @@
-// Recipe.ts
-
 export interface Ingredient {
-    ingredient: string;
-    quantity?: number;
-    unit?: string;
+  ingredient: string;
+  quantity: number | string;
+  unit?: string;
 }
 
 export interface Recipe {
-    id: number;
-    name: string;
-    servings: number;
-    ingredients: Ingredient[];
-    time: number;
-    description: string;
-    appliance: string;
-    ustensils: string[];
+  id: number;
+  name: string;
+  servings: number;
+  ingredients: Ingredient[];
+  time: number;
+  description: string;
+  appliance: string;
+  ustensils: string[];
 }
